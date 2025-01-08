f5f29561ab070a4b8f9d90581602c7295843c2a9
MinecraftedGaming <82008247+MinecraftedGaming@users.noreply.github.com>
2025-01-08 08:51:10 -0600

    Add files via upload

---
+++ Bank/SetBal.lua
@@ -0,0 +1,20 @@
+if settings.get("card.option.2") == false then
+    print("transaction service is currently offline")
+    goto stop
+end
+
+print("enter card number:")
+local card = read()
+if fs.exists("cards/" .. card) == true then
+    print("enter balance:")
+    local bal = read()
+    local file = fs.open("cards/" .. card .. "/CB.hlx", "w")
+    file.write(bal)
+    file.close()
+    local file = fs.open("cards/" .. card .. "/log.hlx", "a")
+    file.writeLine("(" .. os.day("ingame") .. ") - balance set to $" .. bal .. " by SetBal on " .. os.getComputerID())
+    file.close()
+else
+    print("card not found")
+end
+::stop::
