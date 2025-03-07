3e13ab2194768d065ef1daf26c5ceac4a83aa750
MinecraftedGaming <82008247+MinecraftedGaming@users.noreply.github.com>
2025-03-07 07:24:51 -0600

    Update CardMake.lua

--- Bank/CardMake.lua
+++ Bank/CardMake.lua
@@ -4,17 +4,17 @@ local EXP = 0
 local CVV = 0
 
 --check if new cards are allowed
-if settings.get("card.option.1") == false then
-    print("new cards are not allowed at this time")
+if settings.get("bank.option.1") == false then
+    printError("new cards are not allowed at this time")
     goto stop
 end
 
 --generate the card details
-print("enter custom card number")
+print("set new card number")
 CNUM = read()
-print("enter custom card EXP. current date: " .. os.day("ingame"))
+print("set card EXP. current date: " .. os.date("local"))
 EXP = read()
-print("enter custom CVV")
+print("set CVV")
 CVV = read()
 
 --check for repeated card number
@@ -22,8 +22,17 @@ if fs.exists("cards/" .. CNUM) == false then
     print("please enter card holder name")
     local Owner = read()
     fs.makeDir("cards/" .. CNUM)
-    local file = fs.open("cards/" .. CNUM .. "/info.hlx", "w")
-    file.write(CNUM .. " / " .. EXP .. " / " .. CVV .. " / " .. Owner)
+    local file = fs.open("cards/" .. CNUM .. "/info/cvv.hlx", "w")
+    file.write(CVV)
+    file.close()
+    local file = fs.open("cards/" .. CNUM .. "/info/exp.hlx", "w")
+    file.write(EXP)
+    file.close()
+    local file = fs.open("cards/" .. CNUM .. "/info/num.hlx", "w")
+    file.write(CNUM)
+    file.close()
+    local file = fs.open("cards/" .. CNUM .. "/info/holder.hlx", "w")
+    file.write(Owner)
     file.close()
     local file = fs.open("cards/" .. CNUM .. "/CB.hlx", "w")
     file.write("0")
