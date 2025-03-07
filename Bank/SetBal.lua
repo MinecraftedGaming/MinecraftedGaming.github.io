1bde8720a0cef9b7d1a45a43a5fe232f9f4cf197
MinecraftedGaming <82008247+MinecraftedGaming@users.noreply.github.com>
2025-03-07 07:25:31 -0600

    Update SetBal.lua

--- Bank/SetBal.lua
+++ Bank/SetBal.lua
@@ -1,3 +1,22 @@
+local arguments = { ... }
+
+if arguments[1] ~= nil then
+    local card = arguments[1]
+    local bal = arguments[2]
+    if fs.exists("cards/" .. card) == true then
+        local file = fs.open("cards/" .. card .. "/CB.hlx", "w")
+        file.write(bal)
+        file.close()
+        local file = fs.open("cards/" .. card .. "/log.hlx", "a")
+        file.writeLine("(" .. os.day("local") .. ") - balance set to $" .. bal .. " by SetBal on " .. os.getComputerID())
+        file.close()
+        print("set balance of " .. card .. " to $" .. bal)
+        goto stop
+    else
+        print("card not found")
+    end
+end
+
 if settings.get("card.option.2") == false then
     print("transaction service is currently offline")
     goto stop
@@ -12,8 +31,9 @@ if fs.exists("cards/" .. card) == true then
     file.write(bal)
     file.close()
     local file = fs.open("cards/" .. card .. "/log.hlx", "a")
-    file.writeLine("(" .. os.day("ingame") .. ") - balance set to $" .. bal .. " by SetBal on " .. os.getComputerID())
+    file.writeLine("(" .. os.day("local") .. ") - balance set to $" .. bal .. " by SetBal on " .. os.getComputerID())
     file.close()
+    print("set balance of " .. card .. " to $" .. bal)
 else
     print("card not found")
 end
